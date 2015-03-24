5b96dc290c1f929fa9941d8f1fd3045a71d549ee
Antoine Augusti <antoine.augusti@insa-rouen.fr>
2015-03-24 11:18:35 +0100

    Add configuration files for Scala and Spark

---
+++ custom/scala.zsh
@@ -0,0 +1,3 @@
+export SCALA_VERSION="2.11.6"
+export SCALA_HOME=/usr/local/src/scala/scala-$SCALA_VERSION
+export PATH=$SCALA_HOME/bin:$PATH
---
+++ custom/spark.zsh
@@ -0,0 +1,2 @@
+export SPARK_BIN=~/spark-1.2.1-bin-hadoop2.4/bin
+export PATH=$SPARK_BIN:$PATH
