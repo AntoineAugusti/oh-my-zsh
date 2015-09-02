dc8874480348bb061f44c51c01f8959d94b311ac
Antoine Augusti <antoine.augusti@insa-rouen.fr>
2015-09-02 13:09:21 +0200

    no_proxy variable for INSA

--- custom/proxy-insa.zsh
+++ custom/proxy-insa.zsh
@@ -2,3 +2,4 @@ export http_proxy='http://cachemad.insa-rouen.fr:3128'
 export https_proxy='http://cachemad.insa-rouen.fr:3128'
 export npm_config_proxy='cachemad.insa-rouen.fr:3128'
 export npm_config_https_proxy='cachemad.insa-rouen.fr:3128'
+export no_proxy=".insa-rouen.fr,.pic,localhost"
