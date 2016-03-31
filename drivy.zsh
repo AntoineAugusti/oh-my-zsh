8cf3eb0c0c39be13e0940c8cafd8396a16ea0d75
Antoine Augusti <antoine@drivy.com>
2016-03-31 14:51:36 +0200

    Work on aliases at Drivy

---
+++ drivy.zsh
@@ -0,0 +1,2 @@
+alias tunnelAirflow="ssh -L4242:127.0.0.1:8080 ubuntu@redash.antoine-augusti.fr"
+alias redashEmbulk="ssh ubuntu@redash.antoine-augusti.fr -t 'sudo su embulk -c \"cd /home/embulk; /bin/bash\"'"
