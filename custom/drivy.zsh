862588be663da1322c9a87e355bf7a772f1b8059
Antoine Augusti <antoine@drivy.com>
2016-11-18 11:21:08 +0100

    More Drivy aliases

--- custom/drivy.zsh
+++ custom/drivy.zsh
@@ -1,10 +1,9 @@
 if [ $host = "Antoine-MacBook-Pro" ]; then
     # Airflow
-    alias tunnelAirflow="ssh -L4242:127.0.0.1:8080 redash"
+    alias airflowTunnel="ssh -L4242:127.0.0.1:8080 airflow"
+    alias airflowEmbulk="ssh airflow-embulk -t 'cd etl; bash --login'"
 
     # Redah
-    alias redashUbuntu="ssh redash"
-    alias redashEmbulk="ssh redash-embulk -t 'cd etl; bash --login'"
     alias redash="ssh redash -t 'cd /opt/redash/current; /bin/bash'"
     alias redash-socks="ssh -D 1337 -C -N redash -v"
 
@@ -20,6 +19,7 @@ if [ $host = "Antoine-MacBook-Pro" ]; then
     alias etl="cd $HOME/Documents/etl"
     alias dags="cd $HOME/Documents/etl/dags"
     alias udf="cd $HOME/Documents/etl/redshift-udfs"
+    alias crails="cd $HOME/Documents/drivy-rails/"
 
     # Autoload Embulk env variables
     source $HOME/embulk-env.sh
@@ -27,4 +27,7 @@ if [ $host = "Antoine-MacBook-Pro" ]; then
     # Rails
     alias dstart='foreman start -f Procfile.dev'
     alias drestart='pgrep unicorn | xargs kill -USR2'
+
+    # Aliases
+    alias reqFile='rm -f $HOME/Desktop/req.sql && stt $HOME/Desktop/req.sql'
 fi
