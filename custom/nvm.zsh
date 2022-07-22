872cde303e077b0f6d5db99e8e2f9e83b7387b2f
Antoine Augusti <antoine.augusti@gmail.com>
2022-07-22 11:07:56 +0200

    Update config

--- custom/nvm.zsh
+++
@@ -1,8 +0,0 @@
-export NVM_DIR="$HOME/.nvm"
-mkdir -p $NVM_DIR
-
-# This loads nvm
-[ -s "/usr/local/opt/nvm/nvm.sh" ] && . "/usr/local/opt/nvm/nvm.sh"
-
-# This loads nvm bash_completion
-[ -s "/usr/local/opt/nvm/etc/bash_completion.d/nvm" ] && . "/usr/local/opt/nvm/etc/bash_completion.d/nvm"
--- custom/zsh.zsh
+++ custom/zsh.zsh
@@ -2,4 +2,5 @@ export ZSH_THEME='gnzh'
 export ZSH_CUSTOM="$HOME/oh-my-zsh/custom"
 export ENABLE_CORRECTION='true'
 export COMPLETION_WAITING_DOTS='true'
-export ZSH_PLUGINS=(git last-working-dir sudo web-search sublime)
+# May have a conflict with the `plugins` variable in ~/.zshrc
+export ZSH_PLUGINS=(git last-working-dir sudo web-search sublime asdf)
